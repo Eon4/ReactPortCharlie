ac96d5e468a13924f22dad9e75859a3763477823
Cecilia <109989926+Eon4@users.noreply.github.com>
2024-04-23 16:00:55 +0200

    Update 6 - Added ScrollToTop
    - Added a simple ScrollToTop button.

--- onepageP/src/components/Navigation/Nav.jsx
+++ onepageP/src/components/Navigation/Nav.jsx
@@ -1,15 +1,19 @@
 
 import style from './Nav.module.scss'
+import ScrollToTop from "react-scroll-to-top";
+
 
 export const Navigation = () => {
 
   return (
     <nav className={style.navStyle}>
       <div>
-      <a href="#CL">CL</a>
+      <a href="#Eon4">Eon4</a>
         <a href="#projects">Projects</a>
         <a href="#about">About</a>
         <a href="#contact">Contact</a>
+        <ScrollToTop smooth />
+
       </div>
     </nav>
   );
