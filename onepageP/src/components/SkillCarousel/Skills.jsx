e526aa43327ab28476d9468a3f562d541aaaf2f3
Cecilia <109989926+Eon4@users.noreply.github.com>
2024-05-02 20:01:03 +0200

    Update 15 - Change of skill carousel.
    - Changed the style.
    - Added fade in and fade out effect.
    - Added dots for navigation.
    - Changes automatically.
    - Not yet completed.

--- onepageP/src/components/SkillCarousel/Skills.jsx
+++ onepageP/src/components/SkillCarousel/Skills.jsx
@@ -1,5 +1,6 @@
 import style from '../SkillCarousel/Skills.module.scss';
 import { useEffect, useState } from "react";
+import React from 'react';
 import reactIcon from '../../assets/img/Skillicons/reacticon.svg';
 import HTMLIcon from '../../assets/img/Skillicons/html.svg';
 import JSIcon from '../../assets/img/Skillicons/css.svg';
@@ -16,33 +17,69 @@ import PSIcon from '../../assets/img/Skillicons/PS.svg';
 import XDIcon from '../../assets/img/Skillicons/XD.svg';
 import GimpIcon from '../../assets/img/Skillicons/gimp.svg';
 
+const icons = [
+  reactIcon,
+  HTMLIcon,
+  JSIcon,
+  CSSIcon,
+  gitIcon,
+  GraphQLIcon,
+  SassIcon,
+  VSIcon,
+  NodeJSIcon,
+  PostmanIcon,
+  MySQLIcon,
+  FigmaIcon,
+  PSIcon,
+  XDIcon,
+  GimpIcon
+];
+
 const ShowMeYourSkills = () => {
-  const [hoveredIndex, setHoveredIndex] = useState(null);
+  const [currentIconIndex, setCurrentIconIndex] = useState(0);
+  const [fadeIn, setFadeIn] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setFadeIn(true); 
+      setCurrentIconIndex((prevIndex) => (prevIndex + 1) % icons.length);
+    }, 1500); 
+
+    return () => clearTimeout(timer);
+  }, [currentIconIndex]);
+
+  const handleDotClick = (index) => {
+    setCurrentIconIndex(index);
+    setFadeIn(true); // Start of the fade-in for the selected icon
+  };
 
   return (
-    <div className={style.carouselContainer}>
-      <div className={style.carousel}>
-        <div className={style.scope}>
-          <span style={{ '--i': 1 }}><img src={reactIcon} alt="React" /></span>
-          <span style={{ '--i': 2 }}><img src={HTMLIcon} alt="HTML" /></span>
-          <span style={{ '--i': 3 }}><img src={JSIcon} alt="JavaScript" /></span>
-          <span style={{ '--i': 4 }}><img src={CSSIcon} alt="CSS" /></span>
-          <span style={{ '--i': 5 }}><img src={gitIcon} alt="Git" /></span>
-          <span style={{ '--i': 6 }}><img src={GraphQLIcon} alt="GraphQL" /></span>
-          <span style={{ '--i': 7 }}><img src={SassIcon} alt="Sass" /></span>
-          <span style={{ '--i': 8 }}><img src={VSIcon} alt="VS Code" /></span>
-          <span style={{ '--i': 9 }}><img src={NodeJSIcon} alt="Node.js" /></span>
-          <span style={{ '--i': 10 }}><img src={PostmanIcon} alt="Postman" /></span>
-          <span style={{ '--i': 11 }}><img src={MySQLIcon} alt="MySQL" /></span>
-          <span style={{ '--i': 12 }}><img src={FigmaIcon} alt="Figma" /></span>
-          <span style={{ '--i': 13 }}><img src={PSIcon} alt="Photoshop" /></span>
-          <span style={{ '--i': 14 }}><img src={XDIcon} alt="Adobe XD" /></span>
-          <span style={{ '--i': 15 }}><img src={GimpIcon} alt="GIMP" /></span>
+    <div className={style.galleryContainer}>
+      <div className={style.gallery}>
+        <div className={style.myIcons}>
+          <span>
+            {icons.map((icon, index) => (
+              <img
+                key={index}
+                src={icon}
+                alt={`Icon ${index}`}
+                className={(index === currentIconIndex && fadeIn) ? style.fadeIn : style.fadeOut}
+              />
+            ))}
+          </span>
         </div>
+      </div>
+      <div className={style.navigationDots}>
+        {icons.map((_, index) => (
+          <span
+            key={index}
+            className={index === currentIconIndex ? style.active : ''}
+            onClick={() => handleDotClick(index)}
+          />
+        ))}
       </div>
     </div>
   );
 };
 
 export default ShowMeYourSkills;
-
