de1db74309a27a84bb339a50b352ab60c3dd677f
Cecilia <109989926+Eon4@users.noreply.github.com>
2024-05-13 19:43:09 +0200

    Update 21 - Gallery and more
    - Added missing link for projects.
    - Added image for Coffee By James.
    - Added new font family for gallery.

--- onepageP/src/components/ProjectSection/ProjectSection.jsx
+++ onepageP/src/components/ProjectSection/ProjectSection.jsx
@@ -3,30 +3,33 @@ import style from "./ProjectSection.module.scss";
 import SectionImg from '../../assets/img/pcHeader.jpg';
 import flowerTest from '../../assets/img/flowertest.jpg';
 import flower02 from '../../assets/img/flower02.jpg';
-import flower03 from '../../assets/img/flower03.jpg';
+import CoffeeJames from '../../assets/img/Jamesimg.png';
 
 
 export function ProjectSection() {
   // Defining an array of image URLs
-  const imageUrls = [SectionImg, flowerTest, flower02, flower03];
+  const imageUrls = [SectionImg, flowerTest, flower02, CoffeeJames];
 
-  return (
-      <div className={style.galleryContainer}>
-          <div className={style.imageGrid}>
-              {imageUrls.map((imageUrl, index) => (
-                  <div key={index} className={style.gridItem}>
-                      <img src={imageUrl} alt={`Image ${index + 1}`} className={style.image} />
-                      <div className={style.overlay}>
-                          <div>
-                              <h2 className={style.overlayText}>Name</h2>
-                              <p className={style.descText}>Description text</p>
-                              {index === 3 && <a href="#" className={style.seeMoreLink}>Visit Coffee By James</a>}
+// Defining arrays of names and descriptions
+const names = ["Name 1", "Name 2", "Name 3", "Coffee By James"];
+const descriptions = ["Description text 1", "Description text 2", "Description text 3", "First project. Online Coffee Store, with latest design updates overseen by the owner to enable seamless self-management of the webpage."];
 
-                          </div>
-                      </div>
-                  </div>
-              ))}
+return (
+  <div className={style.galleryContainer}>
+    <div className={style.imageGrid}>
+      {imageUrls.map((imageUrl, index) => (
+        <div key={index} className={style.gridItem}>
+          <img id="projects" src={imageUrl} alt={`Image ${index + 1}`} className={style.image} />
+          <div className={style.overlay}>
+            <div>
+              <h2 className={style.overlayText}>{names[index]}</h2>
+              <p className={style.descText}>{descriptions[index]}</p>
+              {index === 3 && <a href="#" className={style.seeMoreLink}>Visit Coffee By James</a>}
+            </div>
           </div>
-      </div>
-  );
+        </div>
+      ))}
+    </div>
+  </div>
+);
 };
