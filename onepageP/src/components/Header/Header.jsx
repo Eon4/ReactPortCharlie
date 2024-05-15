fd2d6efd84f3716f4ded5e5e4785b67fb27f0478
Cecilia <109989926+Eon4@users.noreply.github.com>
2024-05-15 18:58:59 +0200

    Update 24 - Header update
    - Added new hero text.
    - Added typing animation.

--- onepageP/src/components/Header/Header.jsx
+++ onepageP/src/components/Header/Header.jsx
@@ -1,22 +1,37 @@
 import { useEffect, useState } from 'react';
 import style from '../Header/Header.module.scss';
 import Artimage from '../../assets/img/Portimg/artimage.jpg'
-
 export function Header() {
+
+  const [typedText, setTypedText] = useState('');
+  const fullText = "Tomorrow's Websites.";
+
+  useEffect(() => {
+    let currentIndex = 0;
+    const typingTimer = setInterval(() => {
+      if (currentIndex <= fullText.length) {
+        setTypedText(fullText.slice(0, currentIndex));
+        currentIndex++;
+      } else {
+        clearInterval(typingTimer);
+      }
+    }, 150); 
+
+    return () => clearInterval(typingTimer);
+  }, []);
+
   return (
     <header className={style.headerContainer}>
-     
       <div className={style.textContainer}>
         <h1 className={style.headerStyle}>
-          Welcome to<span> my Portfolio</span>
+          Hello!<span> You can call me</span> Cecilia
         </h1>
-        <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-          do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-          enim ad minim veniam, quis nostrud exercitation ullamco laboris ut
-          officia deserunt mollit anim id est laborum."</p>
+        <h2>
+          I craft <span> Digital Solutions</span> for <span>{typedText}</span>
+        </h2>
       </div>
       <div className={style.hero}>
-        <img className={style.myHero} src={Artimage} alt="" />
+        <img className={style.myHero} src={Artimage} alt="heroimage" />
         <div className={style.wavyBorder}></div>
       </div>
     </header>
